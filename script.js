c6da505f8ab41d7fb9318f447d6a1422d4551e42
Nick Whaley <nicholasrwhaley@gmail.com>
2019-08-23 15:20:33 -0700

    create dropdown and start work on eventlistener by class

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function addHero(box, hero){
+
+
+
+//refer to buttons in each box
+let addHeroes = document.getElementById('addHero');
+
+function addHero(){
+  let dmg = event.target.parentElement.parentElement.getElementsByClassName('HeroDmg')[0];
+
+  console.log(dmg)
+  dmg.textContent = 'hi'
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+//add functionality to buttons for each box.
+addHeroes.addEventListener('click', addHero)

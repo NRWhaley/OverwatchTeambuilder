7192191ee582f87504f647da25b684748cb35adb
Nick Whaley <nicholasrwhaley@gmail.com>
2019-08-24 22:26:10 -0700

    attempt adding event listener to options by iteration

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 
 
 window.onload = function(){
-   let damage = document.getElementsByClassName('addHero')
-  for(let i = 0; i < damage.length; i++){
-    damage[i].addEventListener('click', addHero);
+
+  let hero = document.getElementsByClassName('addHero')
+    for(let i = 0; i < hero.length; i++){
+      hero[i].addEventListener('click', addHeroes)
+    }
+
+   let ones = document.getElementsByClassName('One')
+  for(let i = 0; i < ones.length; i++){
+    ones[i].addEventListener('click', addOnes);
   }
+
+
 
 }
 //refer to buttons in each box
 //let addtTwohOne = document.getElementById('addtTwohOne');
 
-function addHero(){
+
+function addHeroes(){
+  let name = event.target.parentElement.parentElement.getElementsByClassName('HeroName')[0];
   let dmg = event.target.parentElement.parentElement.getElementsByClassName('HeroDmg')[0];
 
-  console.log(dmg)
-  dmg.textContent = 'hi'
+  name.textContent = 'Hi'
+  dmg.textContent = 'Bob'
+
+
+}
+
+function addOnes(){
+  let name = event.target.parentElement.parentElement.parentElement.getElementsByClassName('HeroName')[0];
+  let dmg = event.target.parentElement.parentElement.parentElement.getElementsByClassName('HeroDmg')[0];
+
+
+  name.textContent = '1'
+  dmg.textContent = '1'
 
 
 
